8a4cc2acad5e64ebcd46c16bf232cdaf4f9befea
Likil little hero <72366696+likil33@users.noreply.github.com>
2022-01-05 21:44:57 +0530

    Created project
    project created

---
+++ MySwiftStandardsStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySwiftStandardsStore
+//
+//  Created by Santhosh K on 05/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
